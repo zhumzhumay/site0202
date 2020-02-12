112af5c303f37bad4019551e66b998d2a4ead79a
zhumzhumay <aimanakaamon@gmail.com>
2020-02-12 23:18:10 +0300

    first 2

---
+++ msite.py
@@ -0,0 +1,10 @@
+from app import  db, cli, create_app
+from app.models import User, Post, Notification, Message
+
+app = create_app()
+cli.register(app)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+            'Notification': Notification}
